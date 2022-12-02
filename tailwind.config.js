3383bc54d7a1899d4004d5a16478b8cf3e397e9b
Hubert Ryan <hubertryanofficial@gmail.com>
2022-12-02 01:34:20 -0300

    new project and tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#8A4FFF",
+        },
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: require("tailwind-rn/unsupported-core-plugins"),
+};
