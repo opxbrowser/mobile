3151c90a118f9a7ebdc202098e286b4c31788e42
Hubert Ryan <hubertryanofficial@gmail.com>
2022-12-03 01:32:56 -0300

    verifying address added

--- src/components/InputBoss.js
+++ src/components/InputBoss.js
@@ -11,20 +11,22 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 import isAndroid from "../utils/isAndroid";
 
-const InputBoss = (props) => {
+const InputBoss = ({ pressOnSearch, ...rest }) => {
   const tw = useTailwind();
 
   return (
-    <View style={{ marginBottom: !isAndroid() ? 15 : 0 }}>
+    <View>
       <View style={[tw("bg-primary-200 w-full"), { height: 1 }]} />
       <View style={tw("m-4 flex-row items-center")}>
         <TextInput
-          {...props}
+          {...rest}
           style={tw(
             "bg-gray px-6 py-2 rounded-2xl font-wRegular text-base mr-2 flex-1"
           )}
           placeholder="Enter website address"
           placeholderTextColor={tw("text-dark-500").color}
+          autoCapitalize={"none"}
+          autoCorrect={false}
         />
 
         <ButtonBoss
@@ -37,7 +39,11 @@ const InputBoss = (props) => {
           }
           containerStyle={tw("px-6 bg-gray")}
         />
-        <ButtonBoss title="Ir" containerStyle={tw("ml-2")} />
+        <ButtonBoss
+          title="Ir"
+          containerStyle={tw("ml-2")}
+          onPress={pressOnSearch}
+        />
       </View>
     </View>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { useState, useEffect } from "react";
+import { View, SafeAreaView } from "react-native";
 
-import { useSelector } from "react-redux";
+import { WebView } from "react-native-webview";
+
+import { setLastSearch } from "../app/store/slices/navigationSlice";
+import { useSelector, useDispatch } from "react-redux";
 import { useTailwind } from "tailwind-rn";
 
 import EmptyState from "../components/EmptyState";
@@ -9,13 +12,47 @@ import InputBoss from "../components/InputBoss";
 
 const Home = () => {
   const tw = useTailwind();
+  const dispatch = useDispatch();
+
   const lastSearch = useSelector((state) => state.navigation.lastSearch);
 
+  const [searchAddress, setSearchAddress] = useState("");
+
+  useEffect(() => {
+    if (!!lastSearch) {
+      setSearchAddress(lastSearch);
+    }
+  }, [lastSearch]);
+
+  const handleSearchAddress = () => {
+    let newSearchAddress = searchAddress;
+
+    if (
+      !newSearchAddress.includes("https://") ||
+      !newSearchAddress.includes("http://")
+    ) {
+      newSearchAddress = `https://${newSearchAddress}`;
+    }
+
+    console.log(newSearchAddress);
+
+    dispatch(setLastSearch(newSearchAddress));
+  };
+
   return (
-    <View style={tw("flex-1 bg-white")}>
-      {!lastSearch && <EmptyState />}
-      <InputBoss />
-    </View>
+    <SafeAreaView style={tw("flex-1 bg-white")}>
+      <View style={tw("flex-1 bg-white")}>
+        {!lastSearch && <EmptyState />}
+        {lastSearch && (
+          <WebView style={tw("flex-1")} source={{ uri: searchAddress }} />
+        )}
+        <InputBoss
+          value={searchAddress}
+          onChangeText={(text) => setSearchAddress(text)}
+          pressOnSearch={handleSearchAddress}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
