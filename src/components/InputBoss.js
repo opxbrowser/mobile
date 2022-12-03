43c7db78d4fce5b9de519daacb15381e447e090f
Hubert Ryan <hubertryanofficial@gmail.com>
2022-12-03 00:40:19 -0300

    fixed buttons

--- src/components/InputBoss.js
+++ src/components/InputBoss.js
@@ -1,6 +1,8 @@
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { useTailwind } from "tailwind-rn/dist";
 
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
 import isAndroid from "../utils/isAndroid";
 
 const InputBoss = (props) => {
@@ -13,12 +15,21 @@ const InputBoss = (props) => {
         <TextInput
           {...props}
           style={tw(
-            "bg-gray px-6 py-2 rounded-2xl font-wRegular text-lg flex-1 mr-2"
+            "bg-gray px-6 py-2 rounded-2xl font-wRegular text-base flex-1 mr-2"
           )}
           placeholder="Enter website address..."
           placeholderTextColor={tw("text-dark-500").color}
         />
-        <ButtonBoss title="Ir" containerStyle={tw("px-6 bg-gray")} />
+        <ButtonBoss
+          icon={
+            <MaterialCommunityIcons
+              name="chevron-up"
+              color={tw("text-primary").color}
+              size={22}
+            />
+          }
+          containerStyle={tw("px-6 bg-gray")}
+        />
         <ButtonBoss title="Ir" containerStyle={tw("ml-2")} />
       </View>
     </View>
@@ -31,7 +42,11 @@ const ButtonBoss = ({ icon, title, onPress, containerStyle }) => {
   return (
     <TouchableOpacity onPress={onPress}>
       <View style={[tw("bg-primary px-8 py-2 rounded-2xl"), containerStyle]}>
-        <Text style={tw("font-wRegular text-lg text-white")}>{title}</Text>
+        {!title ? (
+          icon
+        ) : (
+          <Text style={tw("font-wRegular text-lg text-white")}>{title}</Text>
+        )}
       </View>
     </TouchableOpacity>
   );
