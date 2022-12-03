718d2bb4c39832ae45a4a87b144dd936c721bec9
Hubert Ryan <hubertryanofficial@gmail.com>
2022-12-03 00:08:17 -0300

    lottie added and first screen started

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ import { useTailwind } from "tailwind-rn";
 const Home = () => {
   const tw = useTailwind();
   return (
-    <View style={tw("flex-1 justify-center")}>
-      <Text>Ola</Text>
+    <View style={tw("flex-1 justify-center items-center")}>
+      <Text style={tw("text-3xl font-wRegular text-primary")}>Ola</Text>
     </View>
   );
 };
