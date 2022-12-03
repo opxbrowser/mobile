3a0b6f4d6e97d81537a664d0620906fe26d29ca0
Hubert Ryan <hubertryanofficial@gmail.com>
2022-12-02 23:36:04 -0300

    tailwind config and redux

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
       colors: {
         primary: {
           DEFAULT: "#8A4FFF",
+          400: "#C3BEF7",
+        },
+        black: {
+          DEFAULT: "#070600",
+        },
+        dark: {
+          DEFAULT: "#B5B4C2",
+          500: "#505050",
+        },
+        gray: {
+          DEFAULT: "#F5F5F5",
         },
       },
     },
