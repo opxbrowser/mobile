efc6ee44eb49adbedfe389576033538f3158622f
Arkadina <imastucia1990@gmail.com>
2023-08-06 00:37:48 -0300

    fix: english sentences & button route

--- src/utils/textDate.js
+++ src/utils/textDate.js
@@ -3,74 +3,74 @@ import { extendMoment } from "moment-range";
 const moment = extendMoment(Moment);
 
 export function getTextTime(timestamp) {
-  if (timestamp && timestamp != "") {
-    const {
-      diffYear,
-      diffMonths,
-      diffDays,
-      diffHours,
-      diffMinutes,
-      diffSeconds,
-    } = getDiffs(timestamp);
+    if (timestamp && timestamp != "") {
+        const {
+            diffYear,
+            diffMonths,
+            diffDays,
+            diffHours,
+            diffMinutes,
+            diffSeconds,
+        } = getDiffs(timestamp);
 
-    let textTime = "";
+        let textTime = "";
 
-    if (diffYear) {
-      textTime = `Há ${diffYear} ano${diffYear > 1 ? "s" : ""} atrás.`;
-    } else if (diffMonths) {
-      textTime = `Há ${diffMonths} ${diffMonths > 1 ? "meses" : "mês"} atrás.`;
-    } else if (diffDays) {
-      if (diffDays < 0) {
-        textTime = `Há um tempo atrás.`;
-      } else {
-        textTime = `Há ${diffDays} ${diffDays > 1 ? "dias" : "dia"} atrás.`;
-      }
-    } else if (diffHours) {
-      if (diffHours < 0) {
-        textTime = `Há um tempo atrás.`;
-      } else {
-        textTime = `Há ${diffHours} ${diffHours > 1 ? "horas" : "hora"} atrás.`;
-      }
-    } else if (diffMinutes) {
-      textTime = `Há ${diffMinutes} ${
-        diffMinutes > 1 ? "minutos" : "minuto"
-      } atrás.`;
-    } else if (diffSeconds > 30) {
-      textTime = `Há ${diffSeconds} ${
-        diffSeconds > 1 ? "secondos" : "segundo"
-      } atrás.`;
-    } else {
-      textTime = `Agora`;
+        if (diffYear) {
+            textTime = `${diffYear} year${diffYear > 1 ? "s" : ""} ago.`;
+        } else if (diffMonths) {
+            textTime = `${diffMonths} month${diffMonths > 1 ? "s" : ""} ago.`;
+        } else if (diffDays) {
+            if (diffDays < 0) {
+                textTime = `A while ago.`;
+            } else {
+                textTime = `${diffDays} day${diffDays > 1 ? "s" : ""} ago.`;
+            }
+        } else if (diffHours) {
+            if (diffHours < 0) {
+                textTime = `A while ago.`;
+            } else {
+                textTime = `${diffHours} hour${diffHours > 1 ? "s" : ""} ago.`;
+            }
+        } else if (diffMinutes) {
+            textTime = `${diffMinutes} minute${
+                diffMinutes > 1 ? "s" : ""
+            } ago.`;
+        } else if (diffSeconds > 30) {
+            textTime = `${diffSeconds} second${
+                diffSeconds > 1 ? "s" : ""
+            } ago.`;
+        } else {
+            textTime = `Now`;
+        }
+
+        return textTime;
     }
-
-    return textTime;
-  }
-  return "";
+    return "";
 }
 
 function getDiffs(time) {
-  const postConfig = moment.utc(time).toDate();
-  const nowConfig = moment.utc(getTimestamp()).toDate();
+    const postConfig = moment.utc(time).toDate();
+    const nowConfig = moment.utc(getTimestamp()).toDate();
 
-  const range = moment.range(postConfig, nowConfig);
-  const diffDays = range.diff("days");
-  const diffMonths = range.diff("months");
-  const diffYear = range.diff("years");
-  const diffHours = range.diff("hours");
-  const diffMinutes = range.diff("minutes");
-  const diffSeconds = range.diff("seconds");
+    const range = moment.range(postConfig, nowConfig);
+    const diffDays = range.diff("days");
+    const diffMonths = range.diff("months");
+    const diffYear = range.diff("years");
+    const diffHours = range.diff("hours");
+    const diffMinutes = range.diff("minutes");
+    const diffSeconds = range.diff("seconds");
 
-  return {
-    diffYear,
-    diffMonths,
-    diffDays,
-    diffHours,
-    diffMinutes,
-    diffSeconds,
-  };
+    return {
+        diffYear,
+        diffMonths,
+        diffDays,
+        diffHours,
+        diffMinutes,
+        diffSeconds,
+    };
 }
 
 export function getTimestamp() {
-  const timestamp = moment().valueOf();
-  return timestamp;
+    const timestamp = moment().valueOf();
+    return timestamp;
 }
