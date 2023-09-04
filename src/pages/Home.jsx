45c4d5a10897a1fafc7e894ec5dfcab29539ae5a
Arkadina <imastucia1990@gmail.com>
2023-09-04 12:52:48 -0300

    feat: not found page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import {
     Keyboard,
     BackHandler,
     ToastAndroid,
+    Text,
 } from "react-native";
 
 import { WebView } from "react-native-webview";
@@ -20,6 +21,8 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import { useTailwind } from "tailwind-rn";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+import MaskedView from "@react-native-masked-view/masked-view";
 
 import OptionsBox from "../components/OptionsBox";
 import EmptyState from "../components/EmptyState";
@@ -46,6 +49,7 @@ const Home = () => {
     const [hideOptions, setHideOptions] = useState(false);
     const [loading, setLoading] = useState(false);
     const [progressValue, setProgressValue] = useState(0);
+    const [hasPageNotFound, setHasPageNotFound] = useState(false);
 
     useEffect(() => {
         if (!!address && address != lastSearch) {
@@ -86,6 +90,7 @@ const Home = () => {
 
     const handleSearchAddress = (newAddress) => {
         let newSearchAddress = newAddress ?? searchAddress;
+        setHasPageNotFound(false);
 
         if (!newSearchAddress && lastSearch) {
             dispatch(setLastSearch(null));
@@ -122,23 +127,38 @@ const Home = () => {
                         behavior={!isAndroid() ? "padding" : "height"}
                     >
                         {!lastSearch && <EmptyState />}
-                        {lastSearch && (
-                            <WebView
-                                style={tw("flex-1")}
-                                source={{ uri: lastSearch }}
-                                onLoadProgress={(e) => {
-                                    setProgressValue(e.nativeEvent.progress);
-                                    if (e.nativeEvent.progress >= 1) {
+                        {lastSearch &&
+                            (hasPageNotFound === false ? (
+                                <WebView
+                                    style={tw("flex-1")}
+                                    source={{ uri: lastSearch }}
+                                    onLoadProgress={(e) => {
+                                        setProgressValue(
+                                            e.nativeEvent.progress
+                                        );
+                                        if (e.nativeEvent.progress >= 1) {
+                                            loadFinished();
+                                        }
+                                    }}
+                                    onLoadEnd={(e) => {
+                                        dispatch(
+                                            setLastSearchData(e.nativeEvent)
+                                        );
+                                        setSearchAddress(e.nativeEvent.url);
                                         loadFinished();
-                                    }
-                                }}
-                                onLoadEnd={(e) => {
-                                    dispatch(setLastSearchData(e.nativeEvent));
-                                    setSearchAddress(e.nativeEvent.url);
-                                    loadFinished();
-                                }}
-                            />
-                        )}
+                                    }}
+                                    onError={(e) => {
+                                        if (
+                                            e.nativeEvent.description ==
+                                            "net::ERR_NAME_NOT_RESOLVED"
+                                        ) {
+                                            setHasPageNotFound(true);
+                                        }
+                                    }}
+                                />
+                            ) : (
+                                <PageNotFound hideOptions={hideOptions} />
+                            ))}
                         <LoadingBar value={progressValue} loading={loading} />
                         <InputBoss
                             value={searchAddress}
@@ -152,6 +172,72 @@ const Home = () => {
                 </View>
             </SafeAreaView>
         </>
+    );
+};
+
+const PageNotFound = ({ hideOptions }) => {
+    const tw = useTailwind();
+
+    if (hideOptions) return <View style={tw("flex-1")}></View>;
+    return (
+        <View style={tw("flex-1 items-center justify-center")}>
+            <MaskedView
+                style={{
+                    flexDirection: "row",
+                    height: "100%",
+                    transform: [
+                        {
+                            scale: 2,
+                        },
+                    ],
+                }}
+                maskElement={
+                    <View
+                        style={{
+                            backgroundColor: "transparent",
+                            flex: 1,
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Text
+                            style={[
+                                tw("font-wBold"),
+                                {
+                                    fontSize: 60,
+                                    color: "black",
+                                    fontWeight: "bold",
+                                },
+                            ]}
+                        >
+                            404
+                        </Text>
+                    </View>
+                }
+            >
+                <LinearGradient
+                    colors={["#8A4FFF", "#8A4FFF", "#FFF", "#FFFFFF"]}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 0, y: 0.82 }}
+                    style={{ flex: 1, height: "100%" }}
+                />
+            </MaskedView>
+            <View
+                style={tw(
+                    "absolute bottom-44 flex justify-center items-center"
+                )}
+            >
+                <Text style={tw("text-dark-400 text-xl mb-2 font-wRegular")}>
+                    Page not found.
+                </Text>
+                <Text
+                    style={tw("text-dark text-base text-center font-wRegular")}
+                >
+                    The page isn't really working or you {`\n`} typed the page
+                    address wrong. Try again!
+                </Text>
+            </View>
+        </View>
     );
 };
 
